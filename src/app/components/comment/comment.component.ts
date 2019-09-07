a89cb00718eab6143ca1b975f91a3fa4fbe8cb70
Keith Andre Rose <andrerose722@gmail.com>
2019-09-07 16:15:45 -0400

    comment component children

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'comment',
@@ -6,10 +6,25 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-@Input() comment;
-  constructor() { }
+  @Input() comment;
+  replies = []
+  constructor(private elRef:ElementRef) { }
 
   ngOnInit() {
+    // console.log(this.comment)
+    const elem = this.elRef.nativeElement as HTMLElement;
+    elem.firstElementChild.classList.add(`d${this.comment.depth}`);
+    if(!this.comment.replies) return
+    if(this.comment.replies === '') return
+    // if(this.comment.replies.kind === "Listing") return
+    const replies : Array<any> = this.comment.replies.data.children;
+    console.log(replies)
+    if(replies[0]) replies.forEach(i => this.replies.push(i.data))
+    
+  }
+
+  toggleCollapsed = (event:Event) => {
+    this.elRef.nativeElement.firstElementChild.classList.add(`collapsed`)
   }
 
 }
